4083908e11fd19d8a5c863b8b690c347e6dc90b4
Chandler Sun <tlwanan@gmail.com>
2026-03-05 17:50:55 +0800

    fix build error

--- src/app/tools/project-schedule/share/[id]/page.tsx
+++ src/app/tools/project-schedule/share/[id]/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+export const runtime = 'edge';
+
 import { useState, useMemo, useCallback, useEffect, useRef } from 'react';
 import { useParams } from 'next/navigation';
 import Link from 'next/link';
